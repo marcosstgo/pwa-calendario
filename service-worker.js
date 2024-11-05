ba8a059ae13f329394ddeb87b63dc20325c624d1
Marcos Santiago <hello@marcossantiago.com>
2024-11-05 18:20:46 -0400

    update

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,27 @@
+const CACHE_NAME = 'mi-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/styles.css',
+  '/assets/script.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('mi-cache-v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icono-192x192.png',
-        '/icono-512x512.png'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  const cacheAllowlist = ['mi-cache-v1'];
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (!cacheAllowlist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
   );
 });
