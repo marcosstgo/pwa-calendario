45239a68a594bbba44fe3ad0da58c4cda47f4e30
Marcos Santiago <hello@marcossantiago.com>
2024-11-05 17:33:04 -0400

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,21 @@
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('mi-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/icono-192x192.png',
-          '/icono-512x512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('mi-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icono-192x192.png',
+        '/icono-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
