b73efe2f3e3c97df7ccc0f90ba1b7e3ac0b90900
Marcos Santiago <hello@marcossantiago.com>
2024-11-05 17:52:56 -0400

    update

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('mi-cache').then(function(cache) {
+    caches.open('mi-cache-v1').then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html',
@@ -8,6 +8,21 @@ self.addEventListener('install', function(event) {
         '/icono-192x192.png',
         '/icono-512x512.png'
       ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const cacheAllowlist = ['mi-cache-v1'];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (!cacheAllowlist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
